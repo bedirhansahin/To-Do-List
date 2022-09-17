2597a7c9886210efa8194711bdac4365fa8f9a6b
Bedirhan Sahin <bedirhannsahin@gmail.com>
2022-09-17 20:10:57 +0300

    index page is arrenged

--- static/script.js
+++ static/script.js
@@ -1,3 +1,21 @@
-$("li").click(function(){
+$("ul").on("click", "li", function(){
   $(this).toggleClass("completed");
 });
+
+// Delete Todo
+$("ul").on("click", "span", function(e){
+  $(this).parent().fadeOut(500, function(){
+    $(this).remove();
+  });
+});
+
+// New Todo
+$("#todo-input").keypress(function(e){
+  if(e.which === 13){
+    var todoText = $(this).val();
+    $(this).val("")
+    $("ul").append("<li>"+"<span><i class='fa fa-trash'></i></span> " + todoText + "</li>");
+    
+  };
+});
+
